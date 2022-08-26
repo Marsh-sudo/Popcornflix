c8c14dd146c16c0b8d11448a8b876f8d7afb8fe9
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-08-26 05:09:46 +0300

    create url paths

---
+++ movie/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('register',views.register_request,name="register"),
+    path('login',views.login_request,name="login"),
+    path("logout", views.logout_request, name= "logout"),
+    path("play/<movie_id>",views.show_video,name="play"),
+    path("upcoming",views.Upcoming,name="upcoming"),
+    path("popular",views.Popular,name="popular"),
+    path("trending",views.Trending,name="trending"),
+    
+    # path("password_reset", views.password_reset_request, name="password_reset"),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
