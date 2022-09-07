a2fa921e3aad5adbcaff7e13663513ecc9d6a06d
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-09-07 03:19:56 +0300

    create password change form

--- movie/forms.py
+++ movie/forms.py
@@ -1,6 +1,8 @@
+from dataclasses import fields
+from pyexpat import model
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
@@ -13,6 +15,13 @@ class RegisterUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class PasswordChangingForm(PasswordChangeForm):
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','type':'password'}))
+    new_password2 = forms.CharField(max_length=100,widget=forms.PasswordInput(attrs={'class':'form-control','type':'password'}))
+    class Meta:
+        model = User
+        fields = ['new_password1','new_password2']
 
 		  
 
