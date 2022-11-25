f7f79f53f7f5d96d39fdde7fbfb2c4b7e5dbc846
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2022-11-25 05:03:00 +0300

    remove unwanted url

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("movie.urls")),
-    path('',include("series.urls")),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name='password_reset_complete'),      
