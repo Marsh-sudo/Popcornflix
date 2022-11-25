518a8f99643aca9fb6a52bf0b751fba01e91e836
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2022-11-25 05:03:54 +0300

    push to github

--- series/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("streams",views.home_TV,name="series"),
-    
-    
-]
--- series/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .siri import  get_airing_today,get_OnAir
-# Create your views here.
-def home_TV(request):
-    On_Air = get_OnAir()
-    airing_today = get_airing_today()
-
-    return render(request,"TV.html",{"OnAir":On_Air,"today":airing_today})
