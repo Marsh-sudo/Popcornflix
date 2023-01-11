c6279e694ff0083389a79927fb197ef1c45729c1
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-01-11 18:17:17 +0300

    create profileform

--- movie/forms.py
+++ movie/forms.py
@@ -1,31 +1,56 @@
-from dataclasses import fields
-from pyexpat import model
 from django import forms
-from django.contrib.auth.models import User
+from .models import Users,Profile
 from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
 
-class RegisterUserForm(UserCreationForm):
+
+passwordInputWidget = {'password':forms.PasswordInput()}
+
+class SignUpForm(UserCreationForm):
+    
     class Meta:
-        model = User
-        fields = ['username','email','password1','password2']
+        model = Users
+        fields = ('username','email')
+        widgets = [passwordInputWidget]
+
+        USERNAME_FIELD = 'username'
 
     def save(self, commit=True):
-        user = super(RegisterUserForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
+        user = super(SignUpForm,self).save(commit=False)
+        user.username = self.cleaned_data['username']
         if commit:
             user.save()
         return user
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Users
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
 
 class PasswordChangingForm(PasswordChangeForm):
     new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','type':'password'}))
     new_password2 = forms.CharField(max_length=100,widget=forms.PasswordInput(attrs={'class':'form-control','type':'password'}))
     class Meta:
-        model = User
+        model = Users
         fields = ['new_password1','new_password2']
 
-		  
 
-class LoginUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username','password1']
+# class LoginUserForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ['username,password']
+#         widgets = [passwordInputWidget]
