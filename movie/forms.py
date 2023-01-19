38121e159d1051335f6aa8dd4a382e37501c700f
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-01-19 03:45:35 +0300

    create signup and login form

--- movie/forms.py
+++ movie/forms.py
@@ -1,25 +1,84 @@
 from django import forms
 from .models import Users,Profile
-from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm,AuthenticationForm
 
 
 passwordInputWidget = {'password':forms.PasswordInput()}
 
-class SignUpForm(UserCreationForm):
-    
+class RegisterForm(UserCreationForm):
+    # fields we want to include and customize in our form
+    first_name = forms.CharField(max_length=100,
+                                 required=True,
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name',
+                                                               'class': 'form-control',
+                                                               }))
+    last_name = forms.CharField(max_length=100,
+                                required=True,
+                                widget=forms.TextInput(attrs={'placeholder': 'Last Name',
+                                                              'class': 'form-control',
+                                                              }))
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'placeholder': 'Email',
+                                                           'class': 'form-control',
+                                                           }))
+    password1 = forms.CharField(max_length=50,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                  'class': 'form-control',
+                                                                  'data-toggle': 'password',
+                                                                  'id': 'password',
+                                                                  }))
+    password2 = forms.CharField(max_length=50,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password',
+                                                                  'class': 'form-control',
+                                                                  'data-toggle': 'password',
+                                                                  'id': 'password',
+                                                                  }))
+
     class Meta:
         model = Users
-        fields = ('username','email')
-        widgets = [passwordInputWidget]
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
-        USERNAME_FIELD = 'username'
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    password = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 'id': 'password',
+                                                                 'name': 'password',
+                                                                 }))
+    remember_me = forms.BooleanField(required=False)
 
-    def save(self, commit=True):
-        user = super(SignUpForm,self).save(commit=False)
-        user.username = self.cleaned_data['username']
-        if commit:
-            user.save()
-        return user
+    class Meta:
+        model = Users
+        fields = ['username', 'password', 'remember_me']
+# class SignUpForm(UserCreationForm):
+    
+#     class Meta:
+#         model = Users
+#         fields = ('username','email')
+#         widgets = [passwordInputWidget]
+
+#         USERNAME_FIELD = 'username'
+
+#     def save(self, commit=True):
+#         user = super(SignUpForm,self).save(commit=False)
+#         user.username = self.cleaned_data['username']
+#         if commit:
+#             user.save()
+#         return user
 
 class UpdateUserForm(forms.ModelForm):
     username = forms.CharField(max_length=100,
