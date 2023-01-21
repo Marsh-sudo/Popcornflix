6c0ec5c0d4fc0f4308fadfa6b19a95bcceba620b
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-01-21 17:47:00 +0300

    add new paths

--- movie/urls.py
+++ movie/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from movie.views import ProfileView,RegisterView,CustomLoginView
+from movie.views import ProfileView,CustomLoginView,RegisterView
 from .forms import LoginForm
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='users-register'),
     path("logout", views.logout_request, name= "logout"),
     path("play/<movie_id>",views.show_video,name="play"),
     path("upcoming",views.Upcoming,name="upcoming"),
@@ -17,6 +17,9 @@ urlpatterns = [
     path("Updateprofile",views.profile_update,name="Updateprofile"),
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='login.html',
                                            authentication_form=LoginForm), name='login'),
+   
+   
+    
 ]
 
 if settings.DEBUG:
