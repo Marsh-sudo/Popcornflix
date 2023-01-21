9eb8979fdee64f644e82191dfe52cd66bdcd6c8f
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-01-21 17:45:32 +0300

    configure paths

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from movie.views import ResetPasswordView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("movie.urls")),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
