e9329badb7fcbe3fd06cdd131f1500761caaa3f3
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-01-21 17:48:17 +0300

    delete user model

--- movie/models.py
+++ movie/models.py
@@ -1,28 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User,AbstractBaseUser,UserManager
+from django.contrib.auth.models import User
 from PIL import Image
 
 # Create your models here.
-class Users(AbstractBaseUser):
-    username = models.CharField(max_length=100,unique=True)
-    email = models.CharField(max_length=90)
-    bio = models.TextField(max_length=100,blank=True)
-    is_admin = models.BooleanField(default=False)
-    password = models.CharField(max_length=150)
-    profile_pic = models.ImageField(upload_to='images/')
-
-    USERNAME_FIELD = 'username'
-
-    @property
-    def is_staff(self):
-        return self.is_admin
-
-    objects = UserManager()
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
