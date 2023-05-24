b8e182f86af602766b2b49f3f3345575c2dc01cc
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-05-24 04:44:42 +0300

    add session cookie age

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%@1zt*)xdzyq1yaq777dd68e+t@_ie0(g80)890_46pk9+vwcd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
